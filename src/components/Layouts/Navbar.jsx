75322963904a77259577d7464ab9c38f9ca14094
fairuz akhdan <fairuzakhdan01@gmail.com>
2024-11-14 15:46:15 +0700

    add input navbar

---
+++ src/components/Layouts/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import InputNotes from "../Elements/input"
+const Navbar = () => {
+    return (
+        <div>
+            <InputNotes type="text" name="search" placeholder="Cari Catatan ..."/>
+        </div>
+    )
+}
